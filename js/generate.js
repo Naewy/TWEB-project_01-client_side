3f7fa6435b2c6bf6f7e99a567891b897f87b231e
Newtt <m.chatelan@gmail.com>
2017-10-04 13:59:31 +0200

    tableau okay, css a fignoler

--- js/generate.js
+++ js/generate.js
@@ -2,6 +2,9 @@
 $(document).ready(function () {
   let xhttp = new XMLHttpRequest();
 
+  // Settings
+  const NB_ENTRIES = 10;
+
   // Makes the request to get the data
   xhttp.open("GET", "http://localhost:3000/data");
   xhttp.responseType = "json";
@@ -9,13 +12,16 @@ $(document).ready(function () {
 
   // Callback function when the state is changed
   xhttp.onreadystatechange = () => {
+
     // If everything is okay
     if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200) {
 
       let object = xhttp.response;
-      document.getElementById("entreprise").innerHTML = object;
 
-      console.log("Data : " + object[0].name);
+      // Add dynamically all the repos to the table
+      for (let i = 0; i < object.length; i++) {
+        $("table").append(`<tr><td>${object[i].name}</td><td id="nb_issues">${object[i].nb_issues}</td></tr>`);
+      }
     }
   };
 
