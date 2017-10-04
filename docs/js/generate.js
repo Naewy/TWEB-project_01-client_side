e171068d207b8728a232f85e2079a64fbeea4114
Naewy <katomey@gmail.com>
2017-10-04 15:16:19 +0200

    Test heroku

---
+++ docs/js/generate.js
@@ -0,0 +1,29 @@
+//TODO parser le json que je recois en un objet
+$(document).ready(function () {
+  let xhttp = new XMLHttpRequest();
+
+  // Settings
+  const NB_ENTRIES = 10;
+
+  // Makes the request to get the data
+  xhttp.open("GET", "https://sheltered-wave-21391.herokuapp.com/data");
+  xhttp.responseType = "json";
+  xhttp.send();
+
+  // Callback function when the state is changed
+  xhttp.onreadystatechange = () => {
+
+    // If everything is okay
+    if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200) {
+
+      let object = xhttp.response;
+
+      // Add dynamically all the repos to the table
+      for (let i = 0; i < object.length; i++) {
+        $("table").append(`<tr><td>${object[i].name}</td><td id="nb_issues">${object[i].nb_issues}</td></tr>`);
+      }
+    }
+  };
+
+  $("#nom").text("Test");
+});
